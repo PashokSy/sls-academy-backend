ed1d315b233baed789bc418ca83d065482c16dba
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-21 17:50:52 +0300

    refactor(perf):  db connection
    refactor db connection: use Pool instead of Client to avoid db overheat and better performance for future query requests

--- 01_Auth_API/db/connect.js
+++ 01_Auth_API/db/connect.js
@@ -1,25 +1,27 @@
 require('dotenv').config({ path: '../.env' });
-const { Client } = require('pg');
-const client = new Client({
+const { CustomError } = require('../errors');
+
+const { Pool } = require('pg');
+const pool = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DB_NAME,
+  max: 30,
+  connectionTimeoutMillis: 60000,
+  idleTimeoutMillis: 0,
 });
 
-const dbCheck = async () => {
+const dbConnectionCheck = async () => {
   try {
-    await client.connect();
-    const res = await client.query('SELECT $1::text as message', [
+    const result = await pool.query('SELECT $1::text as message', [
       'Connection to DB established',
     ]);
-    console.log(res.rows[0].message);
+    console.log(result.rows[0].message);
   } catch (error) {
-    throw error;
-  } finally {
-    client.end();
+    throw new CustomError(`Failed connection to database \n ${error}`);
   }
 };
 
-module.exports = dbCheck;
+module.exports = { dbConnectionCheck, pool };
